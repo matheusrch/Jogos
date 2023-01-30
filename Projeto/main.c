e147132ccd44b7fdc24d50e667c071e44465852f
Matheus Rocha <matheusacrocha7@gmail.com>
2023-01-29 22:01:49 -0300

    Progresso no projeto

--- Projeto/main.c
+++ Projeto/main.c
@@ -3,13 +3,17 @@
 #include <SDL2/SDL_ttf.h>
 #include <assert.h>
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
 int AUX_WaitEventTimeoutCount(SDL_Event* evt, int* ms);
 
 int main (int argc, char* args[])
 {
+    /* INICIALIZAÇÃO */
     SDL_Init(SDL_INIT_EVERYTHING);
     IMG_Init(0);
+    TTF_Init();
 
     SDL_Window* win = SDL_CreateWindow("SPACE INVADERS",
                         SDL_WINDOWPOS_UNDEFINED,
@@ -17,26 +21,32 @@ int main (int argc, char* args[])
                         550, 500, SDL_WINDOW_SHOWN
                       );
     SDL_Renderer* ren = SDL_CreateRenderer(win, -1, 0);
+
     SDL_Texture* img = IMG_LoadTexture(ren, "sprites3.png");
     assert(img != NULL);
 
+    TTF_Font* fnt = TTF_OpenFont("../tiny.ttf", 40);
+    assert(fnt != NULL);
 
+    srand(time(NULL));
+
+
+    /* EXECUÇÃO */
     SDL_Rect jogador = { 250,400, 50,50 };
     SDL_Rect spriteNave = { 0,0, 490,230 };
     SDL_Rect projetil;
+
+    SDL_Rect inimigo = { 300,120, 50,50 };
+
+    int score = 0;
+    SDL_Rect scoreRect = { 10,5, 160,40 };
     
     int atirando = 0;
 
     int espera = 10;
     int running = 1;
     while (running) {
-        SDL_SetRenderDrawColor(ren, 0x00,0x00,0x00,0x00);
-        SDL_RenderClear(ren);
-        SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
-        SDL_RenderFillRect(ren, &projetil);
-        SDL_RenderCopy(ren, img, &spriteNave, &jogador);
-        SDL_RenderPresent(ren);
-
+        /* EVENTOS */
         SDL_Event evt;
         int isevt = AUX_WaitEventTimeoutCount(&evt, &espera);
         if (isevt) {
@@ -65,10 +75,7 @@ int main (int argc, char* args[])
                     case SDLK_SPACE:
                         if (!atirando) {
                             atirando = 1;
-                            projetil.x = jogador.x + 23;
-                            projetil.y = jogador.y - 15;
-                            projetil.w = 4;
-                            projetil.h = 15;
+                            projetil = (SDL_Rect) { jogador.x + 23,jogador.y - 15, 4,15 };
                         }
                         break;
                 }
@@ -85,13 +92,58 @@ int main (int argc, char* args[])
                 projetil.y = -20;
                 atirando = 0;
             }
+
+            inimigo.x += rand() % 2 ? -1 : 1;
         }
+
+
+        /* COLISÕES */
+        if (SDL_HasIntersection(&projetil, &inimigo)) {
+            score += 10;
+            projetil.y = -20;
+        }
+
+        if (SDL_HasIntersection(&jogador, &inimigo)) {
+            score = 0;
+            jogador = (SDL_Rect) { 250,400, 50,50 };
+        }
+
+
+        /* PONTUAÇÃO */
+        char str[14] = "Score: ";
+        char scoreChar[8];
+        sprintf(scoreChar, "%d", score);
+        strcat(str, scoreChar);
+        SDL_Surface* sfc = TTF_RenderText_Blended(fnt, str, (SDL_Color) {0xFF,0xFF,0xFF,0xFF});
+        assert(sfc != NULL);
+        SDL_Texture* txt = SDL_CreateTextureFromSurface(ren, sfc);
+        assert(txt != NULL);
+        SDL_FreeSurface(sfc);
+
+
+        /* RENDERIZAÇÃO */
+        SDL_SetRenderDrawColor(ren, 0x00,0x00,0x00,0x00);
+        SDL_RenderClear(ren);
+        SDL_RenderCopy(ren, txt, NULL, &scoreRect);
+        SDL_DestroyTexture(txt);
+        // SDL_SetRenderDrawColor(ren, 0x00,0xFF,0x00,0x00);
+        SDL_RenderFillRect(ren, &jogador);
+        SDL_RenderCopy(ren, img, &spriteNave, &jogador);
+        // SDL_SetRenderDrawColor(ren, 0xFF,0x00,0x00,0x00);
+        SDL_RenderFillRect(ren, &inimigo);
+        SDL_RenderCopy(ren, img, &spriteNave, &inimigo);
+        SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
+        SDL_RenderFillRect(ren, &projetil);
+        SDL_RenderPresent(ren);
     }
 
 
+    /* FINALIZAÇÃO */
     SDL_DestroyTexture(img);
+    TTF_CloseFont(fnt);
     SDL_DestroyRenderer(ren);
     SDL_DestroyWindow(win);
+    TTF_Quit();
     SDL_Quit();
 }
 
